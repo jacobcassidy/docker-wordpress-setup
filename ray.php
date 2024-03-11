2e41fe54409a92c0b2bc2aa41b2b18d476c64c82
Jacob Cassidy <jacob@jacobcassidy.com>
2024-03-11 18:58:20 +0700

    Add the `ray.php` file

---
+++ ray.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    /*
+    * This settings controls whether data should be sent to Ray.
+    */
+    'enable' => true,
+
+    /*
+     *  The host used to communicate with the Ray app.
+     */
+    'host' => 'host.docker.internal',
+
+    /*
+     *  The port number used to communicate with the Ray app.
+     */
+    'port' => 23517,
+
+    /*
+     *  Absolute base path for your sites or projects in Homestead, Vagrant, Docker, or another remote development server.
+     */
+    'remote_path' => '/var/www',
+
+    /*
+     *  Absolute base path for your sites or projects on your local computer where your IDE or code editor is running on.
+     */
+    'local_path' => getenv_docker('FULL_LOCAL_PATH', ''),
+];
